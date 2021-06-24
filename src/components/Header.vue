7412616bf5db6300730da97486840554dd7ee595
candido-idea-home <candidoluiz097@gmail.com>
2021-06-24 15:58:10 -0300

    feat: Adiciona componente Header ao App

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <header class="header">
+            <h2>Todo List</h2>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.header{
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+    padding: 15px;
+    color: #fff;
+    background-color: rgb(63, 102, 104);
+}
+</style>
