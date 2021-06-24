88fd3a33c73ae01abff1e4699f08439d58da0c4e
candido-idea-home <candidoluiz097@gmail.com>
2021-06-24 16:01:39 -0300

    feat: Adiciona componente TodoItems ao App

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <section class="todo-items">
+            <h1>Aqui vai ficar os items do To-Do</h1>
+        </section>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+.todo-items{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
