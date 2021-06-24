40714b6eba685818a941a3ab1d4c2feee5352180
candido-idea-home <candidoluiz097@gmail.com>
2021-06-24 16:00:52 -0300

    feat: Adiciona componente SideBar ao App

---
+++ src/components/SideBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <aside class="sideBar">
+            <div class="">All</div>
+            <div>Active</div>
+            <div>Completed</div>
+        </aside>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.sideBar{
+    background-color: firebrick;
+    color: #fff;
+}
+</style>
