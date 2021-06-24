4cc12e6d3f7ad128e4ed172a93c4679bdb720daf
candido-idea-home <candidoluiz097@gmail.com>
2021-06-24 16:00:19 -0300

    feat: Adiciona componente Footer ao App

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+        <div>
+            Feito para testar o Vuex!!!
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.footer{
+    display: flex;
+    justify-content: center;
+    padding: 15px;
+    background-color: #1c1c1c;
+    color: #ccc;
+}
+</style>
