9f532a28bae186182dc2287ea598fd048949de30
candido-idea-home <candidoluiz097@gmail.com>
2021-06-24 16:54:17 -0300

    refactor: Altera o nome do componente TodoItems para TodoItemsList

--- src/components/TodoItemsList.vue
+++ src/components/TodoItemsList.vue
@@ -1,22 +1,26 @@
 <template>
     <div>
         <section class="todo-items">
-            <h1>Aqui vai ficar os items do To-Do</h1>
+            <AddTodoItem />
         </section>
         
     </div>
 </template>
 
 <script>
+import AddTodoItem from './AddTodoIdem.vue';
 
 export default {
-
+    components:{
+        AddTodoItem
+    }
 }
 </script>
 
 <style>
 .todo-items{
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
 }
