81986f31bfb8422f20d1b4c78a42c2dfafdb3aaf
candido-idea-home <candidoluiz097@gmail.com>
2021-06-24 16:24:19 -0300

    style-sheet: Altera o estilo das opções do SideBar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,8 @@
 <template>
     <aside class="sideBar">
-        <div class="">All</div>
-        <div>Active</div>
-        <div>Completed</div>
+        <div class="items-option">All</div>
+        <div class="items-option">Active</div>
+        <div class="items-option">Completed</div>
     </aside>
 </template>
 
@@ -16,5 +16,16 @@ export default {
 .sideBar{
     background-color: firebrick;
     color: #fff;
+}
+.items-option{
+    padding: 6px 10px;
+    margin: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    font-weight: bold;
+}
+
+.items-option:hover {
+    background-color: #c7676787;
 }
 </style>
