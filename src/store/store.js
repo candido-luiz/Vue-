dfca130c7a9209d3bf8f8501352476a71fe50c01
candido-idea-home <candidoluiz097@gmail.com>
2021-06-25 18:26:37 -0300

    feat: Adiciona states, mutations e getters ao store
    Para o controle das opções da SideBar funcionar, foi preciso
    adicionar novos states, mutations e getters para controlar a
    exibição dos items (All items, Active Items e Completed Items)

--- src/store/store.js
+++ src/store/store.js
@@ -5,12 +5,23 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        items: []
+        items: [],
+
+        activeItems: [],
+        showActiveItems: false,
+       
+        completedItems: [],
+        showCompletedItems: false
     },
 
     mutations: {
+
         addItem(state, payload){
             state.items.push(payload.item);
+
+            //Assim que adicionar um novo item automaticamente
+            //ele passa a ser parte dos itens ativos (activeItems)
+            state.activeItems.push(payload.item);
         },
 
         removeItem(state, id){
@@ -19,6 +30,70 @@ export const store = new Vuex.Store({
             })
 
             state.items = filteredItems;
+        },
+
+        addActiveItem(state,item){
+            state.activeItems.push(item);
+
+            //remover da lista de activeItems:
+            let filteredCompletedItems = state.completedItems.filter((it) => {
+                return it.id != item.id ;
+            })
+
+            state.completedItems = filteredCompletedItems;
+        },
+
+        addCompletedItem(state,item){
+            state.completedItems.push(item);
+
+            //remover da lista de activeItems:
+            let filteredActiveItems = state.activeItems.filter((it) => {
+                return it.id != item.id ;
+            })
+
+            state.activeItems = filteredActiveItems;
+        },
+
+        showAllItems(state){
+            state.showCompletedItems = false;
+            state.showActiveItems = false;
+        },
+
+        showActiveItems(state){
+            if(!state.showActiveItems){
+
+                state.showCompletedItems = false;
+                state.showActiveItems = true;
+
+            }
+        },
+
+        showCompletedItems(state){
+            if(!state.showCompletedItems){
+
+                state.showActiveItems = false;
+                state.showCompletedItems = true;
+            }
         }
+    },
+
+    getters:{
+        getActiveItems(state){
+            let orderedActiveItems = state.activeItems.sort((a,b) => {
+                return a.id - b.id;
+            });
+
+            return orderedActiveItems;
+        },
+
+        getCompletedItems(state){
+            let orderedCompletedItems = state.completedItems.sort((a,b) => {
+                return a.id - b.id;
+            });
+
+            return orderedCompletedItems;
+        },
+
+
     }
 })
